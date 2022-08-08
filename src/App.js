a767e00b5fd9d3f9f44af4b69d41be407fcf27d6
Christina Greenwood <greenwoodc@me.com>
2022-08-08 10:13:46 -0600

    pushing full page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import NavBar from "./components/NavBar"
+import Main from "./components/Main";
+
+function App() {
+    return (
+        <div className="container">
+            <NavBar />
+            <Main />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react"
+// import logo from "../images/react.png";
+
+
+function NavBar() {
+    return (
+        <nav className="header">
+            <img src="src/images/react.png" alt="logo" style={{width:"30px"}}/>
+        
+            <h3 className="header-text">React Facts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
+
+export default NavBar
